d18c257b3ad3cadd15de74238b0d387ac9a66698
WMXPY <wm@mengw.io>
2021-05-25 20:09:23 -0500

    add state cow data

--- src/structure/state.ts
+++ src/structure/state.ts
@@ -10,4 +10,24 @@ import { SaveStructureInstance } from "./instance";
 export type SaveStateCOWData = {
 
     readonly m_WaitForRespawn: SaveStructureInstance;
+    readonly m_PlayerInventory: {
+        readonly m_ContainerBackpack: {
+            readonly m_ID: "Backpack";
+            readonly m_ItemCounts: {
+                readonly conTinderBox: number;
+                readonly townTeleport: number;
+                readonly questShovel: number;
+                readonly bluntSmithHammer: number;
+                readonly shieldblacksmith: number;
+                readonly shieldbucklerwood: number;
+                readonly conRum: number;
+                readonly bluntBeastman: number;
+                readonly $id: string;
+            };
+        };
+    };
+};
+
+export type SaveStateCSData = {
+
 };
